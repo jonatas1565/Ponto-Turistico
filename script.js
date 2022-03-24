47844a0bb25b14ccf09f08b0d569d44fdb1608e8
jonatas1565 <jonatasferreira330@gmail.com>
2022-03-24 16:51:01 -0300

    2 atualização

---
+++ script.js
@@ -0,0 +1,13 @@
+function acao(){
+
+    let modal = document.querySelector('.modal-1')
+
+    modal.style.display = 'block';
+}
+
+function fechar(){
+
+    let modal = document.querySelector('.modal-1')
+
+    modal.style.display = 'none';
+}
