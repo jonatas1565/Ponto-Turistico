54ccb3c6d23194cda9abab52f4be88113cbbec92
jonatas1565 <jonatasferreira330@gmail.com>
2022-03-25 23:40:44 -0300

    slide funcionando, falta apenas organizar passador

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
+//modal1
 function acao(){
 
-    let modal = document.querySelector('.modal-1')
+    let modal = document.querySelector(".modal-1")
 
     modal.style.display = 'block';
 }
@@ -11,3 +13,33 @@ function fechar(){
 
     modal.style.display = 'none';
 }
+//Modal2
+function acao2(){
+
+    let modal = document.querySelector('.modal-2')
+
+    modal.style.display = 'block';
+}
+
+function fechar2(){
+
+    let modal = document.querySelector('.modal-2')
+
+   modal.style.display = 'none';
+}
+//modal3
+function acao3(){
+
+    let modal = document.querySelector('.modal-3')
+
+    modal.style.display = 'block';
+}
+
+function fechar3(){
+
+    let modal = document.querySelector('.modal-3')
+
+   modal.style.display = 'none';
+}
+//modal4
+
