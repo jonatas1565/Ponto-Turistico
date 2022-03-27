35f9f1bc87c00031d5421ce670f15fd6f12321d6
jonatas1565 <jonatasferreira330@gmail.com>
2022-03-27 17:00:44 -0300

    responsividade ok

--- script.js
+++ script.js
@@ -81,5 +81,20 @@ let slider = tns({
   autoplay : true,
   autoplayButtonOutput : false,
   "mouseDrag" : true,
+
+  "responsive": {
+    
+    "300": {
+      "items": 1.2,
+      "controls": true,
+      "edgePadding": 30,
+      autoplay : true,
+    autoplayButtonOutput : false,
+    "mouseDrag" : true,
+    },
+    "600": {
+        "items": 3,
+    }
+    }
   
 })
